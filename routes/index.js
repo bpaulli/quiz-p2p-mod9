de43a6a55ccc721890f216bbe8e29cce9cc336d1
Bernardo <bpaulli@hotmail.com>
2015-06-12 16:03:35 -0300

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+/// <reference path="../typings/express/express.d.ts"/>
+
 var express = require('express');
 var router = express.Router();
 
-var quizController = require('../controllers/quiz_controller');
-var commentController = require('../controllers/comment_controller');
-var sessionController = require('../controllers/session_controller');
+var quizController 		= require('../controllers/quiz_controller');
+var commentController 	= require('../controllers/comment_controller');
+var sessionController 	= require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -15,7 +17,9 @@ router.get('/author', function(req, res) {
 	res.render('author', {errors: []});
 });
 
-router.param('quizId', quizController.load);
+router.param('quizId', 		quizController.load);
+router.param('commentId', 	commentController.load);
+
 
 
 // Definicion de rutas de //login
@@ -37,5 +41,6 @@ router.delete('/quizes/:quizId(\\d+)',     	sessionController.loginRequired, qui
 // Definición de rutas de /Comments
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.newcomment);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
